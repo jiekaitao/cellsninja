f7c134dd6897de439b3abbefe0210d20806249db
Jack Tao <jietao@rivieraschools.com>
2021-10-07 09:50:12 -0400

    f

--- imports/header.php
+++ imports/header.php
@@ -32,3 +32,32 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
    </head>
+   
+      <!-- body -->
+      <body class="main-layout">
+
+   	<!-- header section start -->
+	<div class="header_section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="logo" style='font-size: larger;'><b><h4 style="color: red;">Organelle<span style='color: gray;'>Trail</span></h4></b>
+				</div>
+				<div class="col-sm-9">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                           <a class="nav-item nav-link" href="index.php">Home</a>
+                           <a class="nav-item nav-link" href="singleplayer-game.php">Singleplayer Game</a>
+                           <a class="nav-item nav-link" href="sources.php">Sources Used</a>
+                           <a class="nav-item nav-link last" href="#"><img src="images/search_icon.png"></a>
+                           <a class="nav-item nav-link last" href="contact.html"><img src="images/shop_icon.png"></a>
+                        </div>
+                    </div>
+                    </nav>
+				</div>
+			</div>
+		</div>
