89eede9818a24b520ee734ef0cf4fbf90ffe4863
Jack Tao <jietao@rivieraschools.com>
2021-10-07 10:04:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/imports/header.php');
 	    <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="0"></li>
             </ol>
 
             <!-- Wrapper for slides -->
