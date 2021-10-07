5e6c398f2833a116bbac03e1468b69e81412a59c
Jack Tao <jietao@rivieraschools.com>
2021-10-07 10:19:55 -0400

    f

--- index.php
+++ index.php
@@ -5,39 +5,59 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/imports/header.php');
 		<div class="banner_section">
 			<div class="container-fluid">
 				<section class="slide-wrapper">
-    <div class="container-fluid">
-	    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0"></li>
-            </ol>
+				<div class="container-fluid">
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0"></li>
+					</ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="active">
-                    <div class="row">
-					<div class="col-sm-2 padding_0">
-						<p class="mens_taital">Organelle Trail Live!</p>
-						<div class="page_no">1/2</div>
-						<p class="mens_taital_2">Men Shoes</p>
-					</div>
-					<div class="col-sm-5">
-						<div class="banner_taital">
-							<h1 class="banner_text">Waiting on Players</h1>
-							<h1 class="mens_text"><strong>Cells Ninja</strong></h1>
-							<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<button class="buy_bt">Buy Now</button>
-							<button class="more_bt">See More</button>
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<div class="active">
+							<div class="row">
+							<div class="col-sm-2 padding_0">
+								<!--p class="mens_taital">Organelle Trail Live!</p-->
+								<div class="page_no">#$#$/16</div>
+								<p class="mens_taital_2">Men Shoes</p>
+							</div>
+							<div class="col-sm-5">
+								<div class="banner_taital">
+									<h1 class="banner_text">Waiting on Players</h1>
+									<h1 class="mens_text"><strong>Cells Ninja</strong></h1>
+									<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+									<button class="buy_bt">Buy Now</button>
+									<button class="more_bt">See More</button>
+								</div>
+							</div>
+							<div class="col-sm-5">
+								<div class="shoes_img"><img style='border-radius: 15px;' src="images_game/index_cell.jpg"></div>
+							</div>
 						</div>
 					</div>
-					<div class="col-sm-5">
-						<div class="shoes_img"><img style='border-radius: 15px;' src="images_game/index_cell.jpg"></div>
-					</div>
-				</div>
-                </div>
                 </div>
             </div>
         </div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
     </div>
 </section>			
 </div>
