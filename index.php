258a30b23e67594b5f390c074b01d5725db4bf8a
Jack Tao <jietao@rivieraschools.com>
2021-10-07 10:10:32 -0400

    f

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/imports/header.php');
                 <div class="active">
                     <div class="row">
 					<div class="col-sm-2 padding_0">
-						<p class="mens_taital">Men Shoes</p>
-						<div class="page_no">0/2</div>
+						<p class="mens_taital">Organelle Trail Live!</p>
+						<div class="page_no">1/2</div>
 						<p class="mens_taital_2">Men Shoes</p>
 					</div>
 					<div class="col-sm-5">
 						<div class="banner_taital">
-							<h1 class="banner_text">New Running Shoes </h1>
+							<h1 class="banner_text">New Running Shoes</h1>
 							<h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
 							<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 							<button class="buy_bt">Buy Now</button>
@@ -35,68 +35,6 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/imports/header.php');
 					</div>
 				</div>
                 </div>
-                <div class="carousel-item">
-                    <div class="row">
-					<div class="col-sm-2 padding_0">
-						<p class="mens_taital">Men Shoes</p>
-						<div class="page_no">0/2</div>
-						<p class="mens_taital_2">Men Shoes</p>
-					</div>
-					<div class="col-sm-5">
-						<div class="banner_taital">
-							<h1 class="banner_text">New Running Shoes </h1>
-							<h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
-							<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<button class="buy_bt">Buy Now</button>
-							<button class="more_bt">See More</button>
-						</div>
-					</div>
-					<div class="col-sm-5">
-						<div class="shoes_img"><img src="images/running-shoes.png"></div>
-					</div>
-				</div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-					<div class="col-sm-2 padding_0">
-						<p class="mens_taital">Men Shoes</p>
-						<div class="page_no">0/2</div>
-						<p class="mens_taital_2">Men Shoes</p>
-					</div>
-					<div class="col-sm-5">
-						<div class="banner_taital">
-							<h1 class="banner_text">New Running Shoes </h1>
-							<h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
-							<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<button class="buy_bt">Buy Now</button>
-							<button class="more_bt">See More</button>
-						</div>
-					</div>
-					<div class="col-sm-5">
-						<div class="shoes_img"><img src="images/running-shoes.png"></div>
-					</div>
-				</div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-					<div class="col-sm-2 padding_0">
-						<p class="mens_taital">Men Shoes</p>
-						<div class="page_no">0/2</div>
-						<p class="mens_taital_2">Men Shoes</p>
-					</div>
-					<div class="col-sm-5">
-						<div class="banner_taital">
-							<h1 class="banner_text">New Running Shoes </h1>
-							<h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
-							<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							<button class="buy_bt">Buy Now</button>
-							<button class="more_bt">See More</button>
-						</div>
-					</div>
-					<div class="col-sm-5">
-						<div class="shoes_img"><img src="images/running-shoes.png"></div>
-					</div>
-				</div>
                 </div>
             </div>
         </div>
