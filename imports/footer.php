9e1bca91348ea6813f721e745b6d68f582b80460
Jack Tao <jietao@rivieraschools.com>
2021-10-11 12:23:04 -0400

    Update footer.php

--- imports/footer.php
+++ imports/footer.php
@@ -79,21 +79,20 @@ $_SESSION['account']="null";
 ?>
 <script>
 $.toast({
-  text: "Study Club does not exist with this domain!", // Text that is to be shown in the toast
-  heading: 'Note', // Optional heading to be shown on the toast
-  icon: 'error', // Type of toast icon
-  showHideTransition: 'slide', // fade, slide or plain
-  allowToastClose: true, // Boolean value true or false
-  hideAfter: 20000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
-  stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
-  position: 'bottom-left', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
-  textAlign: 'left',  // Text alignment i.e. left, right or center
-  loader: true,  // Whether to show loader or not. True by default
-  loaderBg: '#9ec600',  // Background color of the toast loader
+    text: "Success! Your new unique user ID is ( <?php echo $_SESSION['user_id']; ?> ) You can restore your game session using this code.", // Text that is to be shown in the toast
+    heading: 'Success', // Optional heading to be shown on the toast
+    icon: 'success', // Type of toast icon
+    showHideTransition: 'slide', // fade, slide or plain
+    allowToastClose: true, // Boolean value true or false
+    hideAfter: 30000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+    position: 'bottom-left', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+    textAlign: 'left',  // Text alignment i.e. left, right or center
+    loader: true,  // Whether to show loader or not. True by default
+    loaderBg: '#9ec600',  // Background color of the toast loader
 });
 </script>
 <?php } ?>
-
 
 
 
