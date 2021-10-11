fa87f47a1d982595d5a23c5986c2fe87112ed09b
Jack Tao <jietao@rivieraschools.com>
2021-10-11 19:11:35 -0400

    Update updatePlayers.php

--- php/updatePlayers.php
+++ php/updatePlayers.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$playerCNT = $_POST['playerCNT'];
+$playerCNT = $_POST['text'];
 
 $handle = fopen($_SERVER['DOCUMENT_ROOT'].'/handlers/gameRoomcnt.html', "w+") or die("Unable to open file!");
 fwrite($handle, $playerCNT); 
