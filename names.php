9ac16b518a905dfea3a37aa2bb27bb9b794f03eb
Jack Tao <jietao@rivieraschools.com>
2021-10-11 20:00:46 -0400

    Update names.php

--- names.php
+++ names.php
@@ -1,3 +1,12 @@
-Pedro Castillo
-Jie Tao (Jack)
-Ryan Phinney
+<html>
+
+Pedro Castillo <br>
+Jie Tao (Jack)<br>
+Ryan Phinney<br>
+
+<br><br><br>
+
+<img src='https://pbs.twimg.com/media/D1mGo5dXgAAftms.jpg' alt='image of an armadillo with a caption at the top that read "haha yes."'></img> 
+
+
+</html>
