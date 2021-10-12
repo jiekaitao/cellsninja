de09239ff9a888d020eff187375a77f4c17d619f
Jack Tao <jietao@rivieraschools.com>
2021-10-11 20:59:44 -0400

    f

--- game.php
+++ game.php
@@ -4,6 +4,7 @@ session_start();
 //database config/password info
 require_once($_SERVER['DOCUMENT_ROOT']."/imports/config.php");
 
+if(!isset($_SESSION['game_step'])) {
 //clubid
 $sql = "SELECT game_step, money, resets FROM users WHERE user_id = ?";
 $stmt = mysqli_prepare($link, $sql);
@@ -15,11 +16,15 @@ mysqli_stmt_bind_result($stmt, $game_step, $money, $resets);
 mysqli_stmt_fetch($stmt);
 mysqli_stmt_close($stmt);
 
-if($game_step == 0) {
-//Step 0, user is fresh
-
 $_SESSION['game_step'] = $game_step;
 $_SESSION['money'] = $money;
+$_SESSION['resets'] = $resets;
+}
+
+if($game_step == 0) {
+//Step 0, user is fresh
+$_SESSION['game_step'] = $game_step;
+$_SESSION['money'] = 0;
 $_SESSION['resets'] = $resets;
 
 }
