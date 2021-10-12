a8c62da1a3dc2943c46e0e77d73097c107d0642f
Jack Tao <jietao@rivieraschools.com>
2021-10-11 23:50:31 -0400

    Update classPicker.php

--- classPicker.php
+++ classPicker.php
@@ -187,7 +187,7 @@
                         <br>
                         <em>
                             
-                        <iframe src="https://cells.ninja/classProkaryoteInfo.php" style="border:none;height:400px;width:500px;" title="description"></iframe>
+                        <iframe src="https://cells.ninja/classProkaryoteInfo.php" style="border:none;height:400px;width:1000px;" title="description"></iframe>
 
 
 
