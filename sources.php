58ef5cdc90699c0c44f1c3f8a74cfe8b8dbbf624
Jack Tao <jietao@rivieraschools.com>
2021-10-12 09:24:59 -0400

    Update sources.php

--- sources.php
+++ sources.php
@@ -1,94 +1,23 @@
-<?php 
-include_once($_SERVER['DOCUMENT_ROOT'].'/imports/header.php');
-?>
+<html>
 
-		<div class="banner_section">
-			<div class="container-fluid">
-				<section class="slide-wrapper">
-				<div class="container-fluid">
-					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to="0"></li>
-					</ol>
+(AP Textbook) <br>
+https://www.britannica.com/science/nucleus-biology<br>
+https://www.britannica.com/science/membrane-biology<br>
+https://www.britannica.com/science/cell-wall-plant-anatomy<br>
+https://www.britannica.com/science/nucleolus<br>
+https://www.frontiersin.org/articles/10.3389/fpls.2018.00132/full#:~:text=The%20nucleolus%20is%20the%20largest,the%20eukaryotic%20interphase%20cell%20nucleus.&text=The%20nucleolus%20is%20a%20dynamic,rRNA)<br>
+https://biologydictionary.net/nucleoid/<br>
+https://en.wikipedia.org/wiki/Ribosome<br>
+https://www.nature.com/articles/35080045#:~:text=Ribosomes%20have%20two%20main%20functions,proteins%20(r%2Dproteins).<br>
+https://www.technologynetworks.com/cell-science/articles/prokaryotes-vs-eukaryotes-what-are-the-key-differences-336095#:~:text=No%2C%20prokaryotes%20do%20not%20have,(more%20on%20these%20later).<br>
+https://www.britannica.com/science/mitochondrion<br>
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3513836/<br>
+https://www.nationalgeographic.org/encyclopedia/chlorophyll/#:~:text=Chlorophyll's%20job%20in%20a%20plant,glucose%2C%20a%20type%20of%20sugar.<br>
+https://www.frontiersin.org/research-topics/5623/structure-and-function-of-chloroplasts#:~:text=Chloroplasts%20are%20plant%20cell%20organelles,%2C%20membrane%20lipids%2C%20…<br>
+https://www.nature.com/articles/s41573-019-0036-1#:~:text=Lysosomes%20are%20membrane%2Dbound%20organelles,replacement%20therapy%20has%20proved%20successful.<br>
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4009794/#:~:text=Flagellum%20is%20primarily%20a%20motility,flagella%20all%20over%20the%20bacterium).<br>
+https://www.sciencedirect.com/topics/medicine-and-dentistry/smooth-endoplasmic-reticulum#:~:text=The%20smooth%20endoplasmic%20reticulum%20functions,excess%20of%20smooth%20endoplasmic%20reticulum.<br>
 
-					<br>
-					<br>
-					<br>
 
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner">
-						<div class="active">
-							<div class="row">
-							<div class="col-sm-2 padding_0">
-								<!--p class="mens_taital">Organelle Trail Live!</p-->
-								<div class="page_no">#$#$/16</div>
-							</div>
-							<div class="col-sm-5">
-								<div class="banner_taital">
-									<h1 class="banner_text">Waiting on Players</h1>
-									<h1 class="mens_text"><strong>Cells Ninja</strong></h1>
-									<p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-									<button class="buy_bt">Buy Now</button>
-									<button class="more_bt">See More</button>
-								</div>
-							</div>
-							<div class="col-sm-5">
-								<div class="shoes_img"><img style='border-radius: 15px;' src="images_game/index_cell.jpg"></div>
-							</div>
-						</div>
-					</div>
-                </div>
-            </div>
-        </div>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<div class="collection_section">
-    	<div class="container">
-    		<h1 class="new_text"><strong>Racing Boots</strong></h1>
-    	    <p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-    	</div>
-    </div>
-    <div class="collectipn_section_3 layuot_padding">
-    	<div class="container">
-    		<div class="racing_shoes">
-    			<div class="row">
-    				<div class="col-md-8">
-    					<div class="shoes-img3"><img src="images/shoes-img3.png"></div>
-    				</div>
-    				<div class="col-md-4">
-    					<div class="sale_text"><strong>Sale <br><span style="color: #0a0506;">JOGING</span> <br>SHOES</strong></div>
-    					<div class="number_text"><strong>$ <span style="color: #0a0506">100</span></strong></div>
-    					<button class="seemore">See More</button>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </div>
-    </div>
-</section>			
-</div>
-</div>
-</div>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php include_once($_SERVER['DOCUMENT_ROOT'].'/imports/footer.php'); ?>
+</html>
