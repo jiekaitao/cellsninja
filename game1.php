65bfd7f8cc77291d1ca19500270092ec2075c0e2
Jack Tao <jietao@rivieraschools.com>
2021-10-12 01:34:43 -0400

    Update game1.php

--- game1.php
+++ game1.php
@@ -107,7 +107,7 @@
 
             <section onclick="scheduleB()">
                 <div class="bl-box">
-                    <h2>T-800 Organic Cyber Eukaryote From The Perilous Future</h2>
+                    <h2>Avoid Conflict</h2>
                 </div>
 
                 <div class="bl-content">
